d5d1633cbf77e838248914793f57ff3db4480f61
Aman Pathak <155675919+Amanpathak1@users.noreply.github.com>
2024-04-28 09:58:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import re, os, time
 import datetime
 
 class Config:
-    BOT_TOKEN = os.environ.get('7035001463:AAHpv7RPKL3yqwNdT_1tvmtfZ1F-qBetKaQ')
+    BOT_TOKEN = os.environ.get('6797928481:AAFBeTtvTEvSKIDZjt3N331Zh5jhM5uRJZc')
     API_HASH = os.environ.get('4c26e2cb6c6d9d57841b3c5ab6bb7605')
     API_ID = int(os.environ.get('27184026'))
     OWNER_ID = int(os.environ.get('7183515722')) #Admin UserId
